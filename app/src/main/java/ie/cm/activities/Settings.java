c2c5e2d5574c1767296fc5b9eea4003479a14c92
arundasps <arundasps3@gmail.com>
2019-07-10 13:54:23 -0400

    Exercise Commit

---
+++ app/src/main/java/ie/cm/activities/Settings.java
@@ -0,0 +1,14 @@
+package ie.cm.activities;
+
+import android.os.Bundle;
+
+import ie.cm.R;
+
+public class Settings extends Base {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
